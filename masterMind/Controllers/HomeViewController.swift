ee16fb342c76feaaa9387a94ae9a452873c9a940
Elena Zobak <elenazobak@gmail.com>
2023-04-13 16:16:35 -0400

    homeScreen

--- masterMind/Controllers/HomeViewController.swift
+++ masterMind/Controllers/HomeViewController.swift
@@ -2,28 +2,28 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
-    var attempts = 10
-    var codeLength = 4
-    // Declare UI elements
+   
     
     
     let attemptsLabel: UILabel = {
         let label = UILabel()
         label.text = "Attempts:"
-        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.font = UIFont(name: "Futura-Bold", size: 20) // Use Futura-Bold font with size 20
+        label.textColor = UIColor(hex: 0x273c75) // Set text color to #273c75
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     let attemptsCountLabel: UILabel = {
         let label = UILabel()
-        label.text = "0"
-        label.font = UIFont.boldSystemFont(ofSize: 24)
+        label.text = "6"
+        label.font = UIFont(name: "Futura-Bold", size: 23) // Use Futura-Bold font with size 20
+        label.textColor = UIColor(hex: 0x273c75) // Set text color to #273c75
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-    let addButton: UIButton = {
+    let attemptsAddButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(UIImage(systemName: "plus.circle.fill"), for: .normal)
         button.tintColor = UIColor(red: 0.69, green: 0.95, blue: 0.98, alpha: 1.00)
@@ -31,7 +31,7 @@ class HomeViewController: UIViewController {
         return button
     }()
     
-    let subtractButton: UIButton = {
+    let attemptsSubtractButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(UIImage(systemName: "minus.circle.fill"), for: .normal)
         button.tintColor = UIColor(red: 0.69, green: 0.95, blue: 0.98, alpha: 1.00)
@@ -56,7 +56,8 @@ class HomeViewController: UIViewController {
     let codeLengthLabel: UILabel = {
             let label = UILabel()
             label.text = "Code Length:"
-            label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.font = UIFont(name: "Futura-Bold", size: 20) // Use Futura-Bold font with size 20
+        label.textColor = UIColor(hex: 0x273c75) // Set text color to #273c75
             label.translatesAutoresizingMaskIntoConstraints = false
             return label
         }()
@@ -64,7 +65,8 @@ class HomeViewController: UIViewController {
         let codeLengthCountLabel: UILabel = {
             let label = UILabel()
             label.text = "4"
-            label.font = UIFont.boldSystemFont(ofSize: 24)
+            label.font = UIFont(name: "Futura-Bold", size: 23) // Use Futura-Bold font with size 20
+            label.textColor = UIColor(hex: 0x273c75) // Set text color to #273c75
             label.translatesAutoresizingMaskIntoConstraints = false
             return label
         }()
@@ -119,7 +121,7 @@ class HomeViewController: UIViewController {
         view.addSubview(titleLabel)
         
         NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
             titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
         
@@ -131,7 +133,7 @@ class HomeViewController: UIViewController {
         view.addSubview(bulbImageView)
         
         NSLayoutConstraint.activate([
-            bulbImageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20),
+            bulbImageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 40),
             bulbImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             bulbImageView.widthAnchor.constraint(equalToConstant: 100),
             bulbImageView.heightAnchor.constraint(equalToConstant: 100)
@@ -140,41 +142,40 @@ class HomeViewController: UIViewController {
         // Set up attempts label and buttons
         
         // Create the horizontal stack view
-        let stackView = UIStackView(arrangedSubviews: [attemptsLabel, subtractButton, attemptsCountLabel, addButton])
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .horizontal
-        stackView.distribution = .equalSpacing
-        stackView.alignment = .center
+        let attemptsStackView = UIStackView(arrangedSubviews: [attemptsLabel, attemptsSubtractButton, attemptsCountLabel, attemptsAddButton])
+        attemptsStackView.translatesAutoresizingMaskIntoConstraints = false
+        attemptsStackView.axis = .horizontal
+        attemptsStackView.distribution = .equalSpacing
+        attemptsStackView.alignment = .center
 
         // Add the stack view to the attempts view
-        attemptsView.addSubview(stackView)
+        attemptsView.addSubview(attemptsStackView)
         
         
         view.addSubview(attemptsView)
-       // attemptsView.addSubview(attemptsLabel)
-        
-       // attemptsView.addSubview(addButton)
-       // attemptsView.addSubview(attemptsCountLabel)
-       // attemptsView.addSubview(subtractButton)
+       
         
         // Set the constraints for the attempts view
         NSLayoutConstraint.activate([
             attemptsView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            attemptsView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
-            attemptsView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            attemptsView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            attemptsView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            //attemptsView.widthAnchor.constraint(equalToConstant: 150),
             attemptsView.heightAnchor.constraint(equalToConstant: 60),
+            attemptsSubtractButton.leadingAnchor.constraint(equalTo: attemptsLabel.trailingAnchor, constant: 10),
+            attemptsSubtractButton.centerYAnchor.constraint(equalTo: attemptsLabel.centerYAnchor),
         ])
 
         // Set the constraints for the stack view
         NSLayoutConstraint.activate([
-            stackView.centerYAnchor.constraint(equalTo: attemptsView.centerYAnchor),
-            stackView.centerXAnchor.constraint(equalTo: attemptsView.centerXAnchor),
-            stackView.widthAnchor.constraint(equalTo: attemptsView.widthAnchor, multiplier: 0.8),
-            stackView.heightAnchor.constraint(equalTo: attemptsView.heightAnchor)
+            attemptsStackView.centerYAnchor.constraint(equalTo: attemptsView.centerYAnchor),
+            attemptsStackView.centerXAnchor.constraint(equalTo: attemptsView.centerXAnchor),
+            attemptsStackView.widthAnchor.constraint(equalTo: attemptsView.widthAnchor, multiplier: 0.8),
+            attemptsStackView.heightAnchor.constraint(equalTo: attemptsView.heightAnchor)
         ])
         
-        addButton.addTarget(self, action: #selector(addAttempt), for: .touchUpInside)
-        subtractButton.addTarget(self, action: #selector(subtractAttempt), for: .touchUpInside)
+        attemptsAddButton.addTarget(self, action: #selector(addAttempt), for: .touchUpInside)
+        attemptsSubtractButton.addTarget(self, action: #selector(subtractAttempt), for: .touchUpInside)
         
         // Create the horizontal stack view for code length
         let codeLengthStackView = UIStackView(arrangedSubviews: [codeLengthLabel, codeLengthSubtractButton, codeLengthCountLabel, codeLengthAddButton])
@@ -189,13 +190,7 @@ class HomeViewController: UIViewController {
         // Add the code length view to the main view
         view.addSubview(codeLengthView)
 
-        // Set the constraints for the code length view
-        NSLayoutConstraint.activate([
-            codeLengthView.topAnchor.constraint(equalTo: attemptsView.bottomAnchor, constant: 30),
-            codeLengthView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
-            codeLengthView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
-            codeLengthView.heightAnchor.constraint(equalToConstant: 60),
-        ])
+        
 
         // Set the constraints for the stack view for code length
         NSLayoutConstraint.activate([
@@ -204,6 +199,17 @@ class HomeViewController: UIViewController {
             codeLengthStackView.widthAnchor.constraint(equalTo: codeLengthView.widthAnchor, multiplier: 0.8),
             codeLengthStackView.heightAnchor.constraint(equalTo: codeLengthView.heightAnchor)
         ])
+        // Set the constraints for the code length view
+        NSLayoutConstraint.activate([
+            codeLengthView.topAnchor.constraint(equalTo: attemptsView.bottomAnchor, constant: 30),
+            codeLengthView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            codeLengthView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            codeLengthView.heightAnchor.constraint(equalToConstant: 60),
+            codeLengthSubtractButton.leadingAnchor.constraint(equalTo: codeLengthLabel.trailingAnchor, constant: 10),
+                codeLengthSubtractButton.centerYAnchor.constraint(equalTo: codeLengthView.centerYAnchor),
+        ])
+        
+        
 
         // Add targets for the buttons
         codeLengthAddButton.addTarget(self, action: #selector(codeLengthAddButtonTapped), for: .touchUpInside)
@@ -214,8 +220,8 @@ class HomeViewController: UIViewController {
         playButton.setTitle("Play", for: .normal)
         playButton.setTitleColor(.white, for: .normal)
         playButton.backgroundColor = UIColor(red: 0.08, green: 0.84, blue: 0.76, alpha: 1.0) // Teal color
-        playButton.layer.cornerRadius = 20 // Set corner radius to create an oval shape
-        playButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        playButton.layer.cornerRadius = 25 // Set corner radius to create an oval shape
+        playButton.titleLabel?.font = UIFont(name: "Futura-Bold", size: 22)
         playButton.contentHorizontalAlignment = .center
         playButton.contentVerticalAlignment = .center
         playButton.translatesAutoresizingMaskIntoConstraints = false
@@ -226,9 +232,9 @@ class HomeViewController: UIViewController {
         // Add constraints for positioning the play button
         NSLayoutConstraint.activate([
             playButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            playButton.topAnchor.constraint(equalTo: codeLengthSubtractButton.bottomAnchor, constant: 80),
+            playButton.topAnchor.constraint(equalTo: codeLengthSubtractButton.bottomAnchor, constant: 100),
             playButton.widthAnchor.constraint(equalToConstant: 120),
-            playButton.heightAnchor.constraint(equalToConstant: 40)
+            playButton.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
     
@@ -245,11 +251,13 @@ class HomeViewController: UIViewController {
         }
     }
     @objc func codeLengthAddButtonTapped() {
-        codeLengthCount += 1
+        if codeLengthCount < 7 {
+            codeLengthCount += 1
+        }
     }
 
     @objc func codeLengthSubtractButtonTapped() {
-        if codeLengthCount > 1 {
+        if codeLengthCount > 3 {
             codeLengthCount -= 1
         }
     }
@@ -267,5 +275,15 @@ class HomeViewController: UIViewController {
                 gameViewController.gameSettings = GameSettings(code: "", codeLength: codeLengthCount, attempts: attemptsCount)
             }
         }
+    }
+}
+
+
+extension UIColor {
+    convenience init(hex: Int, alpha: CGFloat = 1.0) {
+        let red = CGFloat((hex >> 16) & 0xff) / 255.0
+        let green = CGFloat((hex >> 8) & 0xff) / 255.0
+        let blue = CGFloat(hex & 0xff) / 255.0
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
     }
 }
